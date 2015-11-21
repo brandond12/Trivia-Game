bfcef296b77b4c920587c159b48528223ba3ad62
CreativeCartoon <laurenmachan@hotmail.com>
2015-11-21 14:12:10 -0500

    Created a script to populate the TriviaGame with questions and answers.

---
+++ mySQL Scripts/PopulateDatabase.sql
@@ -0,0 +1,59 @@
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (1, "Who is the author that wrote the Lord of the Rings?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (1, "C. S. Lewis", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (1, "J. R. R. Tolkien", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (1, "George R. R. Martin", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (1, "J. K. Rowling", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (2, "What year was the first Lord of The Rings book published?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (2, "1954", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (2, "1955", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (2, "1956", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (2, "1957", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (3, "What is the name of the leader of the Ringwraiths?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (3, "Witch-king of Gondor", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (3, "Witch-king of Rohan", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (3, "Witch-king of Arthedain", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (3, "Witch-king of Angmar", TRUE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (4, "How many members are there in the Fellowship of the Ring?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (4, "7", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (4, "8", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (4, "9", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (4, "10", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (5, "What day was Bilbo Baggins born on?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (5, "August 14", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (5, "September 22", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (5, "October 20", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (5, "October 30", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (6, "Who is the main villain?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (6, "Glorfindel", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (6, "Fingon", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (6, "Melkor", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (6, "Sauron", TRUE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (7, "Where does Lord of the Rings take place?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (7, "Middle-earth", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (7, "Great Lands", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (7, "Westeros", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (7, "Narnia", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (8, "What year was the movie 'The Lord of the Rings: The Fellowship of the Ring' released?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (8, "2000", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (8, "2001", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (8, "2002", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (8, "2003", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (9, "What is the ring called?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (9, "Gold Ring", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (9, "Dark Ring", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (9, "One Ring", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (9, "Great Ring", FALSE);
+
+INSERT INTO TestQuestions (QuestionNumber, Question) VALUES (10, "Where was the ring forged?");
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (10, "Weathertop", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (10, "Mount Doom", TRUE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (10, "Rivendell", FALSE);
+INSERT INTO TestAnswers (QuestionNumber, Answer, IsCorrect) VALUES (10, "Gondor", FALSE);
