504a0e6c70eb9ef2e96fd4db954ff39b60880cad
CreativeCartoon <laurenmachan@hotmail.com>
2015-11-21 13:43:28 -0500

    Added a MySQL Script Folder and created a script that creates the TriviaGame database with it's tables.

---
+++ mySQL Scripts/TriviaGameDatabaseCreation.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE TriviaGame;
+USE TriviaGame;
+
+CREATE TABLE Users (
+Name VARCHAR(50),
+IsActive BOOL,
+PRIMARY KEY (Name)
+);
+
+CREATE TABLE UserGames (
+GameNumber INT,
+Name VARCHAR(50),
+GameScore INT,
+PRIMARY KEY (GameNumber, Name),
+FOREIGN KEY (Name) REFERENCES Users(Name)
+);
+
+CREATE TABLE TestQuestions (
+QuestionNumber INT,
+Question VARCHAR(300),
+PRIMARY KEY (QuestionNumber)
+);
+
+CREATE TABLE TestAnswers (
+QuestionNumber INT,
+Answer VARCHAR(100),
+IsCorrect BOOL,
+PRIMARY KEY (QuestionNumber, Answer),
+FOREIGN KEY (QuestionNumber) REFERENCES TestQuestions(QuestionNumber),
+INDEX (Answer)
+);
+
+CREATE TABLE UserAnswer (
+GameNumber INT,
+Name VARCHAR(50),
+GameQuestion INT,
+UserAnswer VARCHAR(100),
+AnswerScore INT,
+PRIMARY KEY (GameNumber, Name),
+FOREIGN KEY (GameNumber, Name) REFERENCES UserGames(GameNumber, Name),
+FOREIGN KEY (GameQuestion) REFERENCES TestQuestions(QuestionNumber),
+FOREIGN KEY (UserAnswer) REFERENCES TestAnswers(Answer)
+);
