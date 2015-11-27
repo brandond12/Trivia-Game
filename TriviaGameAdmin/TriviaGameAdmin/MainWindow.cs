6d874e6d7db9c6d815c27ea674aeeda5197652fa
CreativeCartoon <laurenmachan@hotmail.com>
2015-11-26 22:33:23 -0500

    Added method comments and moved code out of the btn_ExportExcel_Click method into the CreateExcelSheet method.

--- TriviaGameAdmin/TriviaGameAdmin/MainWindow.cs
+++ TriviaGameAdmin/TriviaGameAdmin/MainWindow.cs
@@ -61,7 +61,39 @@ namespace TriviaGameAdmin
             leaders.Show();
         }
 
+        /*
+        *METHOD		    :	btn_ExportExcel_Click
+        *
+        *DESCRIPTION	:	Method called when the 'Export Excel' button is clicked
+        *                   Calls a method to create an Excel sheet and chart with data
+        *
+        *PARAMETERS		:	object sender:  Object relaying information on where the event call came from
+        *                   EventArgs e:    Object that contains data about the event
+        *  
+        *RETURNS		:	void
+        *
+        */
         private void btn_ExportExcel_Click(object sender, EventArgs e)
+        {
+            this.CreateExcelSheet();
+        }
+
+        /*
+        *METHOD		    :	CreateExcelSheet
+        *
+        *DESCRIPTION	:	Method creates a workbook with a sheet, and adds the question number, question 
+        *                   text, average correct answer time and percentage of users who guessed correctly 
+        *                   to the sheet
+        *                   This method calls another method to create a chart
+        *                   This method is a modified version of the button1_Click method from
+        *                   https://support.microsoft.com/en-us/kb/302084
+        *
+        *PARAMETERS		:	
+        *  
+        *RETURNS		:	void
+        *
+        */
+        public void CreateExcelSheet()
         {
             Excel.Application oXL;
             Excel._Workbook oWB;
@@ -70,75 +102,75 @@ namespace TriviaGameAdmin
 
             try
             {
-                //Start Excel and get Application object.
+                //start Excel and get Application object
                 oXL = new Excel.Application();
                 oXL.Visible = true;
-
-                //Get a new workbook.
+                //get a new workbook
                 oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
                 oSheet = (Excel._Worksheet)oWB.ActiveSheet;
 
-                //Add table headers going cell by cell.
+                //add table headers going cell by cell
                 oSheet.Cells[1, 1] = "Question Number";
                 oSheet.Cells[1, 2] = "Question Text";
                 oSheet.Cells[1, 3] = "Average Correct Answer Time (s)";
                 oSheet.Cells[1, 4] = "% Who Answered Correctly";
-
-                //Format A1:K1 as bold, vertical alignment = center, and wrapping = true.
+                //format A1:D1 as bold, vertical and horizontal alignment is center, and text wrapping is true
                 oSheet.get_Range("A1", "D1").Font.Bold = true;
                 oSheet.get_Range("A1", "D1").VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
                 oSheet.get_Range("A1", "D1").HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter;
                 oSheet.get_Range("A1", "D1").WrapText = true;
 
+                //send a user command to the service
                 output.WriteLine("GetExcel.");
                 output.Flush();
 
-                // Made this array of type object to store both strings and ints
+                //get the question number and question text
                 Object[,] questionArray = new Object[11, 2];
-
+                //read the question text from the service
                 for (int counter = 0; counter < 10; counter++)
                 {
                     String questionText = input.ReadLine();
+                    //fill an array with the question number and text
                     questionArray[counter, 0] = counter + 1;
                     questionArray[counter, 1] = questionText;
                 }
-
-                //Fill A2:B6 with an array of values (Question # and Question Text).
+                //fill A2:B11 with the question number and question text
                 oSheet.get_Range("A2", "B11").Value2 = questionArray;
                 oRng = oSheet.get_Range("B2", "B11");
                 oRng.EntireColumn.AutoFit();
 
-                // Get the average time to answer correctly
+                //get the average time to answer correctly
                 float[,] averageTimes = new float[10, 10];
+                //read the average times from the service
                 for (int counter = 0; counter < 10; counter++)
                 {
                     float currentAverageTime = float.Parse(input.ReadLine());
+                    //fill an array with the average times
                     averageTimes[counter, 0] = currentAverageTime;
                 }
-
-                // Fill J2:J11 with an array of average times, and autofit the columns
+                //fill C2:C11 with the average time to answer questions correctly
                 oSheet.get_Range("C2", "C11").Value2 = averageTimes;
                 oRng = oSheet.get_Range("C2", "C11");
                 oRng.EntireColumn.AutoFit();
 
-                // Get the percentage of users who answered the question correctly
+                //get the percentage of users who answered the question correctly
                 float[,] percentCorrect = new float[10, 10];
+                //read the percentages from the service
                 for (int counter = 0; counter < 10; counter++)
                 {
                     float currentQuestionPercent = float.Parse(input.ReadLine());
+                    //fill an array with the percentages
                     percentCorrect[counter, 0] = currentQuestionPercent;
                 }
-
-                // Fill K2:K11 with an array of percents, and autofit the column
+                //fill D2:D11 with the percentage of users who answered correctly
                 oSheet.get_Range("D2", "D11").Value2 = percentCorrect;
                 oRng = oSheet.get_Range("D2", "D11");
                 oRng.EntireColumn.AutoFit();
 
-                //Manipulate a variable number of columns for Quarterly Sales Data.
+                ///create a chart
                 DisplayHistogram(oSheet);
 
-                //Make sure Excel is visible and give the user control
-                //of Microsoft Excel's lifetime.
+                //make sure Excel is visible and give the user control of Microsoft Excel's lifetime
                 oXL.Visible = true;
                 oXL.UserControl = true;
             }
@@ -148,8 +180,19 @@ namespace TriviaGameAdmin
             }
         }
 
-        // In addition, a histogram should be created to show the average length of time needed to answer each question correctly
-        // this method is a modified version of the code from https://support.microsoft.com/en-us/kb/302084
+        /*
+        *METHOD		    :	DisplayHistogram
+        *
+        *DESCRIPTION	:	Method creates an Excel chart that displays the average length of time to answer 
+        *                   questions correctly
+        *                   This method is a modified version of the DisplayQuarterlySales method from
+        *                   https://support.microsoft.com/en-us/kb/302084
+        *
+        *PARAMETERS		:	Excel._Worksheet oWS    A worksheet interface  
+        *  
+        *RETURNS		:	void
+        *
+        */
         public void DisplayHistogram(Excel._Worksheet oWS)
         {
             Excel._Workbook oWB;
@@ -159,16 +202,16 @@ namespace TriviaGameAdmin
 
             try
             {
-                //Add a Chart for the selected data.
+                //add a chart for the selected data
                 oWB = (Excel._Workbook)oWS.Parent;
                 oChart = (Excel._Chart)oWB.Charts.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value);
 
-                //Use the ChartWizard to create a new chart from the selected data.
+                //use the ChartWizard to create a new chart from the selected data
                 oRange = oWS.get_Range("B1", "C11");
                 oChart.SetSourceData(oRange, Missing.Value);
-                oChart.ChartWizard(oRange, Excel.XlChartType.xlBarClustered, Missing.Value,
-                    Excel.XlRowCol.xlColumns, Missing.Value, Missing.Value, Missing.Value,
-                    "Average Length of Time to Answer Questions Correctly", "Questions", "Time (seconds)", Missing.Value);
+                oChart.ChartWizard(oRange, Excel.XlChartType.xlBarClustered, Missing.Value, Excel.XlRowCol.xlColumns, 
+                Missing.Value, Missing.Value, Missing.Value, "Average Length of Time to Answer Questions Correctly", 
+                "Questions", "Time (seconds)", Missing.Value);
                 oSeries = (Excel.Series)oChart.SeriesCollection(1);
                 oSeries.XValues = oWS.get_Range("B2", "B11");
             }
